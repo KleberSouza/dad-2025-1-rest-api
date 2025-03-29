304df6b31719acdea471db6feda77b1dee039478
Kleber Souza <kleberjfsouza@gmail.com>
2025-03-28 21:02:18 -0300

    Adicionar arquivos de projeto.

---
+++ apiRest/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace apiRest.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo nome obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo e-mail obrigatório")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+    }
+}
